d7e08537e77c4e8ae2522026452ecceac5221e10
Omar Cely <omarcelyvillate@gmail.com>
2021-06-11 12:52:32 -0500

    Some more beautify v:

--- Vivado_Modules/CameraModules/Tx.v
+++ Vivado_Modules/CameraModules/Tx.v
@@ -9,19 +9,19 @@ output      o_Tx_Active;
 output reg  o_Tx_Serial;
 output      o_Tx_Done;
 
-  parameter CLKS_PER_BIT   = 1085;//434;
-  parameter s_IDLE         = 3'b000;
-  parameter s_TX_START_BIT = 3'b001;
-  parameter s_TX_DATA_BITS = 3'b010;
-  parameter s_TX_STOP_BIT  = 3'b011;
-  parameter s_CLEANUP      = 3'b100;
+parameter s_IDLE         = 3'b000;
+parameter CLKS_PER_BIT   = 1085;//434;
+parameter s_TX_START_BIT = 3'b001;
+parameter s_TX_DATA_BITS = 3'b010;
+parameter s_TX_STOP_BIT  = 3'b011;
+parameter s_CLEANUP      = 3'b100;
 
-  reg [2:0]    r_SM_Main     = 0;
-  reg [10:0]    r_Clock_Count = 0;
-  reg [2:0]    r_Bit_Index   = 0;
-  reg [7:0]    r_Tx_Data     = 0;
-  reg          r_Tx_Done     = 0;
-  reg          r_Tx_Active   = 0;
+reg [2:0]    r_SM_Main     = 0;
+reg [10:0]    r_Clock_Count = 0;
+reg [2:0]    r_Bit_Index   = 0;
+reg [7:0]    r_Tx_Data     = 0;
+reg          r_Tx_Done     = 0;
+reg          r_Tx_Active   = 0;
 
     always @(posedge i_Clock) begin
       case (r_SM_Main)
