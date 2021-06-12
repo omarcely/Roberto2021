b21f59665357098c11049142eac44072c7fe122e
Dportela2001 <85375999+Dportela2001@users.noreply.github.com>
2021-06-11 21:55:28 -0500

    Update Config.ino

--- ESP32/CameraConfig/Config.ino
+++ ESP32/CameraConfig/Config.ino
@@ -1,7 +1,15 @@
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+#define TXD2 17
+#define RXD2 16
+#define cntrl 21
 
-void setup(){
+BluetoothSerial SerialBT;
 
-  /*Before using the code make sure you have connected the following pins of the Camera:
+void setup() {
+    /*Before using the code make sure you have connected the following pins of the Camera:
     Camera SCL to Microcontroler SCL pin
     Camera SDA to Microcontroler SDA pin
     Camera RET to Logic High
@@ -21,12 +29,13 @@ void setup(){
 
   //writeRegister(0x42, 0x12, 0x80, 22, 23, 100)  //Reset register
 
-  writeRegister(0x42, 0x0C, 0x0C, 22, 23, 100)  //Enable Digital Zoom and Down Sampling
-  writeRegister(0x42, 0x72, 0x33, 22, 23, 100)  //Set horizontal and vertical downsampling of 8
-  writeRegister(0x42, 0x70, 0x28, 22, 23, 100)  //Set Scaling XSC
-  writeRegister(0x42, 0x71, 0x28, 22, 23, 100)  //Set Scaling YSC
-  writeRegister(0x42, 0x73, 0x03, 22, 23, 100)  //Set PCLK divide by 8
-  writeRegister(0x42, 0xA2, 0x09, 22, 23, 100)  //Scaling Delay
+  writeRegister(0x42, 0x0C, 0x0C, 22, 23, 100);  //Enable Digital Zoom and Down Sampling
+  writeRegister(0x42, 0x72, 0x33, 22, 23, 100);  //Set horizontal and vertical downsampling of 8
+  writeRegister(0x42, 0x70, 0x28, 22, 23, 100);  //Set Scaling XSC
+  writeRegister(0x42, 0x71, 0x28, 22, 23, 100); //Set Scaling YSC
+  writeRegister(0x42, 0x73, 0x03, 22, 23, 100);  //Set PCLK divide by 8
+  writeRegister(0x42, 0xA2, 0x09, 22, 23, 100);  //Scaling Delay
+  writeRegister(0x42, 0x11, 0x9C, 22, 23, 100);  //Framerate CLKRC ~ 1.5fps
 
 
 
@@ -36,9 +45,33 @@ void setup(){
   Serial.println(readRegister(0x43, 0x71, 22, 23, 100));
   Serial.println(readRegister(0x43, 0x73, 22, 23, 100));
   Serial.println(readRegister(0x43, 0xA2, 22, 23, 100));
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  pinMode(cntrl,INPUT);
+}
 
-  }
+int i=0;
+bool control_in=false;
 
 void loop(){
-
+  if(digitalRead(cntrl) == LOW){ //Only begin to read and print if the control pin has already indicated the begin of the frame
+      if(i==0){
+        if(control_in==false){ //Control variable for only one print of "in"
+        Serial.println("in");
+        control_in=true;
+        }
+      }
+      if(Serial2.available()){ //Send the information to python just if there is any available in serial port
+        Serial.println(Serial2.read());
+        i++;
+      }
+      if(i==9216){
+        i=0;
+        Serial.println("end");
+        control_in=false;
+      }
   }
+  delay(50);
+}
