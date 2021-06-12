0bdba79ad1a475cd99038d6c5529bfee7aaf732d
Omar Cely <omarcelyvillate@gmail.com>
2021-06-11 20:02:03 -0500

    Register configuration kind of ready v:

--- ESP32/CameraConfig/Config.ino
+++ ESP32/CameraConfig/Config.ino
@@ -18,13 +18,25 @@ void setup(){
   digitalWrite(23,HIGH);
   digitalWrite(15,HIGH);
   Serial.begin(9600);
-   if(writeRegister(0x42, 0x12, 0x80, 22, 23, 100)){
-        Serial.println("The register was succesfully overrote");
-      }else{
-        Serial.println("There was an error during the writing process");
-        }
-   Serial.print("Valor del registro: ");
-   Serial.println(readRegister(0x43, 0x12, 22, 23, 100));
+
+  //writeRegister(0x42, 0x12, 0x80, 22, 23, 100)  //Reset register
+
+  writeRegister(0x42, 0x0C, 0x0C, 22, 23, 100)  //Enable Digital Zoom and Down Sampling
+  writeRegister(0x42, 0x72, 0x33, 22, 23, 100)  //Set horizontal and vertical downsampling of 8
+  writeRegister(0x42, 0x70, 0x28, 22, 23, 100)  //Set Scaling XSC
+  writeRegister(0x42, 0x71, 0x28, 22, 23, 100)  //Set Scaling YSC
+  writeRegister(0x42, 0x73, 0x03, 22, 23, 100)  //Set PCLK divide by 8
+  writeRegister(0x42, 0xA2, 0x09, 22, 23, 100)  //Scaling Delay
+
+
+
+  Serial.println(readRegister(0x43, 0x0C, 22, 23, 100));
+  Serial.println(readRegister(0x43, 0x72, 22, 23, 100));
+  Serial.println(readRegister(0x43, 0x70, 22, 23, 100));
+  Serial.println(readRegister(0x43, 0x71, 22, 23, 100));
+  Serial.println(readRegister(0x43, 0x73, 22, 23, 100));
+  Serial.println(readRegister(0x43, 0xA2, 22, 23, 100));
+
   }
 
 void loop(){
