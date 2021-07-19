f9c2f6f2185a24ac3634d84a2a1dd2b795608a13
Juan Felipe González Pardo <38539346+felipeg86@users.noreply.github.com>
2021-07-19 09:12:57 -0500

    Add files via upload

---
+++ ESP32/MainBlock/BrainBlock.ino
@@ -0,0 +1,63 @@
+void brain(){
+    r = SerialBT.read();
+    if(r == 'r'){
+      sendToPhy = true;
+      distance[0] = readUltrasonic(trigger,uts_1);
+      delay(100);
+      distance[1] = readUltrasonic(trigger,uts_2);
+      delay(100); 
+      distance[2] = readUltrasonic(trigger,uts_3);
+      delay(100);
+      currentState = compare(distance);
+    }else if(r == 's'){
+      sendToPhy = false;
+    }else{
+      sendToPhy = sendToPhy;
+    }
+    
+    if(sendToPhy == true){
+        switch(currentState){
+          case 'l':
+            turn(currentState, dutyCycle);
+            currentState = 's';
+          break;
+            
+          case 'f':
+            turn(currentState, dutyCycle); 
+            distance[1] = readUltrasonic(trigger,uts_2);
+            delay(100); 
+            SerialBT.println(distance[1]);
+            distance[2] = readUltrasonic(trigger,uts_3);
+            delay(100);            
+            nextState = compare(distance);
+            if(nextState != currentState){
+                currentState = 's';
+                dutyCycle[0] = topSpeed;
+                dutyCycle[1] = topSpeed;
+            }else{
+                currentState = nextState;
+                if (dutyCycle[0] > minimumSpeed){
+                  dutyCycle[0] = dutyCycle[0] - 20;
+                  dutyCycle[1] = dutyCycle[1] - 20;
+                } 
+            }
+          break;
+          
+          case 'r':
+            turn(currentState, dutyCycle);
+            currentState = 's';
+          break;
+          
+          default: 
+            turn(currentState, dutyCycle);
+            distance[0] = readUltrasonic(trigger,uts_1);
+            delay(100);
+            distance[1] = readUltrasonic(trigger,uts_2);
+            delay(100); 
+            distance[2] = readUltrasonic(trigger,uts_3);
+            delay(100);
+            currentState = compare(distance);
+          break; 
+       }
+       }
+}
